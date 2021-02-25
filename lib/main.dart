40415cb4b6f88094e8d7b81e96fb319fc1689cc6
Rakshithvihaan Badiger <prajwal11302002@gmail.com>
2021-02-26 00:02:39 +0530

    With Medicine reminder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@
 import 'package:blood_donation/Login.dart';
 import 'package:blood_donation/SignUp.dart';
 import 'package:blood_donation/Start.dart';
-import 'package:blood_donation/load.dart';
+// import 'package:blood_donation/load.dart';
+import 'package:blood_donation/screens/add_new_medicine/add_new_medicine.dart';
+import 'package:blood_donation/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 import 'HomePage.dart';
@@ -18,13 +20,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.red),
+        // theme: ThemeData(primaryColor: Colors.red),
+        theme: ThemeData(
+            fontFamily: "Popins",
+            primaryColor: Colors.red,
+            textTheme: TextTheme(
+                headline1: ThemeData.light().textTheme.headline1.copyWith(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 38.0,
+                      fontFamily: "Popins",
+                    ),
+                headline5: ThemeData.light().textTheme.headline1.copyWith(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17.0,
+                      fontFamily: "Popins",
+                    ),
+                headline3: ThemeData.light().textTheme.headline3.copyWith(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20.0,
+                      fontFamily: "Popins",
+                    ))),
         debugShowCheckedModeBanner: false,
         home: HomePage(),
         routes: <String, WidgetBuilder>{
+          // "/": (context) => HomePage(),
           "Login": (BuildContext context) => Login(),
           "SignUp": (BuildContext context) => SignUp(),
           "start": (BuildContext context) => Start(),
+          "/home": (BuildContext context) => Home(),
+          "/add_new_medicine": (BuildContext context) => AddNewMedicine(),
         });
   }
 }
